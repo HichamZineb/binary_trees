7afc72dd8e90b31364e78daf6e8209728f01132b
HichamZineb <hichamelamine@hotmail.com>
2023-08-31 06:35:29 -0700

    update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t right = 0;
 	size_t left = 0;
+	size_t right = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	right = binary_tree_height(tree->right);
-	left = binary_tree_height(tree->left);
+	if (tree)
+	{
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	}
 
-	return ((left > right) ? left + 1 : right + 1);
+	return ((left > right) ? left : right);
 }
